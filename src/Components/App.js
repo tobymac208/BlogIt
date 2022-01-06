07e8f75e2a94f40c32fe8ae2ad9466c6398d66ca
Nik Fernandez <niklasfernandez97@gmail.com>
2022-01-06 12:03:45 -0600

    Fix CSS styling.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
+import Test from './Test';
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<Test />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-    
+    return (
+    <div className="ui menu">
+        <div className='ui container center'>
+            <p>BlogIt &copy; {new Date().getFullYear()}</p>
+        </div>
+    </div>
+    );
 };
 
 export default Footer;
