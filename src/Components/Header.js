e22e54be025f3056cd2b8a8ddb9b4a9ac7f6920a
Nik Fernandez <niklasfernandez97@gmail.com>
2022-01-16 11:43:18 -0600

    Modifies favicon.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,11 @@ const Header = (props) => {
         <div className="ui large secondary inverted pointing menu">
           <a className="item" href="/">Home</a>
           <a className="item">Projects</a>
-          <div className="right item">
+          <a className="item" href="/about">About</a>
+          {/* <div className="right item">
             <a className="ui inverted button">Log in</a>
             <a className="ui inverted button">Sign Up</a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
