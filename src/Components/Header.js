98122d059a46c6663dd36e21003b6a3556abc84b
Nik Fernandez <niklasfernandez97@gmail.com>
2022-01-16 14:05:49 -0600

    Updates links in header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,13 +5,14 @@ const Header = (props) => {
     <div className="ui inverted vertical center aligned segment">
       <div className="ui container">
         <div className="ui large secondary inverted pointing menu">
+          <a className="item header">BlogIt</a>
           <a className="item" href="/">Home</a>
-          <a className="item">Projects</a>
+          <a className="item"  href="/projects">Projects</a>
           <a className="item" href="/about">About</a>
-          {/* <div className="right item">
-            <a className="ui inverted button">Log in</a>
-            <a className="ui inverted button">Sign Up</a>
-          </div> */}
+          <div className="right item">
+            <a className="ui inverted button" href="/login">Log in</a>
+            <a className="ui inverted button" href="/sign-up">Sign Up</a>
+          </div>
         </div>
       </div>
     </div>
