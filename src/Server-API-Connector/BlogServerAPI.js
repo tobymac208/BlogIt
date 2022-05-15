7f0268013e0cd5ed5b49a37fdd836932fcd00ad1
Nik Fernandez <niklasfernandez97@gmail.com>
2022-05-14 22:43:22 -0500

    trying something

--- src/Server-API-Connector/BlogServerAPI.js
+++ src/Server-API-Connector/BlogServerAPI.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:3006/",
+    // baseURL: "http://localhost:3006/",
+    baseURL: "https://fake-rest-nikf.herokuapp.com"
 });
